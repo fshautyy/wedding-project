02ae79c43f5d6e0912998a0135c34cee9211a0c5
Ananda Yudha <yudha@newtronic-solution.com>
2025-11-26 10:59:54 +0700

    setup action gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/wedding-project/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -10,4 +11,7 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        outDir: 'dist',
+  },
 });
